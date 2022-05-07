6aacd6b9a70a620cbb1275c12cd21a773439ba26
Hannah Kwon <49659772+hkwon516@users.noreply.github.com>
2022-05-07 00:11:46 -0700

    Update App.js

--- bruinmatch-frontend/src/App.js
+++ bruinmatch-frontend/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import LogIn from './components/LogIn';
-import SignUp from './components/SignUp';
-import Rec from './components/Rec';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+import Rec from "./components/Rec";
+import Saved from "./components/Saved";
 
 class App extends Component {
   render() {
