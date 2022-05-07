beacb6fce3b894998280706c2092e028b2f50c84
Hannah Kwon <49659772+hkwon516@users.noreply.github.com>
2022-05-07 00:06:35 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#2774AE",
+      },
+      fontFamily: {
+        title: ["menlo"],
+      },
+    },
+  },
+  plugins: [],
+};
