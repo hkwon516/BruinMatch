4967cd4e29b3e495af97498aebd3f3e9fa9b7de1
bella <jchoi1001@g.ucla.edu>
2022-05-19 11:27:06 -0700

    rec cards

--- bruinmatch-frontend/src/components/Profile.js
+++ bruinmatch-frontend/src/components/Profile.js
@@ -7,20 +7,23 @@ import { Link } from "react-router-dom";
 const UserProfile = (props) => {
     const  user  = props.user;
     return(
-              <div className="profile-container ">
-              <div className="ProfilePhoto1"></div>
-              <div className="name1">Name: {user.name}</div>
-              <div className="gender1">Gender: {user.gender}</div>
-              <div className="major1">Major: {user.major}</div>
-              <div className="year1">Year: {user.year}</div>
-              <div className="phone1">Phone: {user.phone}</div>
-              <div className="email1">Email: {user.email}</div>
-              <div className="comments1">Comments:</div>
+              <div className="container">
+                <div className="ProfilePhoto1"></div>
+                <div className="name1">{user.name}</div>
+                <div className="gender1">Gender: {user.gender}</div>
+                <div className="major1">Major: {user.major}</div>
+                <div className="year1">Year: {user.year}</div>
+                <div className="phone1">Phone: {user.phone}</div>
+                <div className="email1">Email: {user.email}</div>
+                <div className="bio1">Bio:</div>
+                <button class="save1">Like</button>
+                <button class="view1">View</button>
+              {/*<div className="comments1">Comments:</div>
               <div className="pref1">Same Gender:</div>
               <div className="pref2">On The Hill:</div>
               <div className="pref3">Alcohol:</div>
               <div className="pref4">Pets:</div>
-              <div className="pref5">Night Owl:</div>
+              <div className="pref5">Night Owl:</div>*/}
               </div>
     )
 };
