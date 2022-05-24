92e2c7705855b63c1a308111c99c2a5c1de0390f
smuppalla <smuppalla521@ucla.edu>
2022-05-23 19:55:55 -0700

    Fixed Saving Feature

--- bruinmatch-frontend/src/components/Profile.js
+++ bruinmatch-frontend/src/components/Profile.js
@@ -5,17 +5,27 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import swal from 'sweetalert';
 
-var user = "";
-var account = "";
 
-function addProfile() {
+function addProfile(user, account) {
+  console.log(account);
+  console.log(user);
   var currUser = account;
   var saved = currUser.savedProfiles;
-  saved.push(user);
-  currUser.savedProfiles = saved;
-  axios
-    .put(`http://localhost:8082/api/users/saved/${account.username}`, currUser)
-    .then((res) => {});
+  var addUser = true;
+  for(var i = 0; i < saved.length; i++){
+    if(saved[i].username === user.username){
+      addUser = false;
+    }
+  }
+  if(addUser){
+    console.log(1);
+    saved.push(user);
+    currUser.savedProfiles = saved;
+    axios
+      .put(`http://localhost:8082/api/users/saved/${account.username}`, currUser)
+      .then((res) => {});
+  }
+  
 }
 
 function ViewButton(username)
@@ -37,10 +47,15 @@ function importAllImages(r) {
 const images = importAllImages(require.context('../../../bruinmatch-backend/images', false, /\.(png|jpe?g|svg)$/));
 
 const UserProfile = (props) => {
-  user = props.user;
-  account = props.account;
-  console.log(account);
+  const user = props.user;
+  const account = props.account;
   let picId = user.username + '.png';
+  var gender = "Male";
+  if(user.gender == 2){
+    gender = "Female";
+  }else if(user.gender == 3){
+    gender = "Not Defined";
+  }
   return (
     <div className="card-container">
       {/* Change Image Routing!! And ProfPic Dimensions */}
@@ -49,12 +64,12 @@ const UserProfile = (props) => {
       
       <img class="ProfilePhoto1" src={images[picId]} />
       <div className="name1">{user.name}</div>
-      <div className="gender1">Gender: {user.gender}</div>
+      <div className="gender1">Gender: {gender}</div>
       <div className="major1">Major: {user.major}</div>
       <div className="year1">Year: {user.year}</div>
       <div className="phone1">Phone: {user.phone}</div>
       <div className="email1">Email: {user.email}</div>
-      <div className="bio1">Bio:</div>
+      <div className="bio1">Bio: {user.bio}</div>
       {/*<button className="save1">
         Like
       </button>*/}
@@ -62,7 +77,8 @@ const UserProfile = (props) => {
         type="button"
         className="save1"
         value="Like"
-        onClick="alert('aaaaaaa')"
+        // onClick="alert('aaaaaaa')"
+        onClick = {() => addProfile(user, account)}
       />
       <input
         type="button"
